239a9714e114934b468c41416296bb2811d0c9cf
frrst-ian <frrstxd@proton.me>
2025-10-30 15:10:45 +0800

    feat: add dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
 import Nav from "./components/Nav/Nav";
 import "./styles/App.css";
+// import { UserContext } from "./context/UserContext";
+// import { useContext } from "react";
 
 const App = () => {
+  // const { user } = useContext(UserContext);
+
   return (
     <BrowserRouter>
       <div className="app">
-          <Nav></Nav>
+        <Nav></Nav>
         <main className="main">
           <AppRoutes></AppRoutes>
         </main>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import styles from "./Nav.module.css";
+import useOutsideClick from "../../hooks/useOutsideClick";
+
+// fix me don't show on auth pages
 
 export default function Nav() {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOutside = () => {
+        setOpen(false);
+    };
+
+    const ref = useOutsideClick(handleClickOutside);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+
     return (
         <div className={styles.navWrapper}>
             <nav>
@@ -12,10 +28,20 @@ export default function Nav() {
                         <a href="/users">Users</a>
                     </li>
                 </ul>
-                <div>
-                    <button>
+                <div className={styles.dropdown}>
+                    <button ref={ref} type="button" onClick={handleOpen}>
                         Account
                     </button>
+                    {open ? (
+                        <ul className={styles.menu}>
+                            <li className={styles.menuItem}>
+                                <button>View profile</button>
+                            </li>
+                            <li className={styles.menuItem}>
+                                <button>Logout</button>
+                            </li>
+                        </ul>
+                    ) : null}
                 </div>
             </nav>
         </div>
